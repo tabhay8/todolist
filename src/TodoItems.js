7725d4029a72a1175a832747f81ec72c135beb97
Sougat Thapa <32903435+tabhay8@users.noreply.github.com>
2023-07-11 21:56:48 -0400

    change in css and added comments

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -3,15 +3,19 @@ import TodoContext from "./TodoContext";
 import FlipMove from "react-flip-move";
 
 function TodoItems() {
+  // Access the items and deleteItem function from the TodoContext using useContext hook
   const { items, deleteItem } = useContext(TodoContext);
 
+  // Function to handle the deletion of an item
   const handleDelete = (key) => {
     deleteItem(key);
   };
 
   return (
     <ul className="theList">
+      {/* Use FlipMove for animated transitions */}
       <FlipMove duration={250} easing="ease-out">
+        {/* Render the list of items */}
         {items.map((item) => (
           <li onClick={() => handleDelete(item.key)} key={item.key}>
             {item.text}
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,12 +6,14 @@ import TodoContext from "./TodoContext";
 function TodoList() {
   const [items, setItems] = useState([]);
 
+  // Load the items from local storage when the component mounts
   useEffect(() => {
     const savedItems = localStorage.getItem("todoItems");
     const parsedItems = savedItems ? JSON.parse(savedItems) : [];
     setItems(parsedItems);
   }, []);
 
+  // Function to handle adding a new item
   const addItem = (e) => {
     e.preventDefault();
     if (inputElement.value !== "") {
@@ -20,16 +22,25 @@ function TodoList() {
         key: Date.now(),
       };
 
+      // Update the items state by adding the new item
       const updatedItems = [...items, newItem];
       setItems(updatedItems);
+
+      // Save the updated items to local storage
       localStorage.setItem("todoItems", JSON.stringify(updatedItems));
+
+      // Clear the input field
       inputElement.value = "";
     }
   };
 
+  // Function to handle deleting an item
   const deleteItem = (key) => {
+    // Filter out the item with the specified key
     const filteredItems = items.filter((item) => item.key !== key);
     setItems(filteredItems);
+
+    // Save the updated items to local storage
     localStorage.setItem("todoItems", JSON.stringify(filteredItems));
   };
 
@@ -39,13 +50,16 @@ function TodoList() {
     <div className="todoListMain">
       <div className="header">
         <form onSubmit={addItem}>
+          {/* Input element for adding new items */}
           <input
             ref={(a) => (inputElement = a)}
             placeholder="Enter your Task"
           />
+          {/* Button to submit the form */}
           <button type="submit">Add</button>
         </form>
       </div>
+      {/* Provide the items and deleteItem function to the TodoItems component */}
       <TodoContext.Provider value={{ items, deleteItem }}>
         <TodoItems />
       </TodoContext.Provider>
